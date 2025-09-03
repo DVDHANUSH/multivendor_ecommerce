58383b79adba8ad90d686efd210574ddc0af115f
DVDHANUSH <dhanush0909@gmail.com>
2025-09-03 05:50:36 +0530

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
 import type { Metadata } from "next";
 import { Inter, Barlow } from "next/font/google";
 import "./globals.css";
-// Theme provider 
 import { ThemeProvider } from "next-themes";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
-const barlowFont = Barlow({ subsets: ["latin"], weight: ["500", "700"], variable: "--font-barlow" });
+const barlowFont = Barlow({
+  subsets: ["latin"],
+  weight: ["500", "700"],
+  variable: "--font-barlow",
+});
+
 export const metadata: Metadata = {
   title: "GoShop - Multi Vendor Ecommerce",
   description: "A multi vendor ecommerce platform built with Next.js and TypeScript",
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,9 +24,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} ${barlowFont.variable}`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        {children}
-        </ThemeProvider>
+        <ClerkProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import ThemeToggle from "@/components/shared/theme-toggle";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
      <div>
-      <div className="w-100 flex justify-end"><ThemeToggle></ThemeToggle></div>
+      <div className="w-100 flex justify-end">
+        <UserButton></UserButton>
+        <ThemeToggle>
+        </ThemeToggle>
+      </div>
       <h1 className="fon-bold text-blue-500 font-barlow">Welcome to the course</h1>
         <Button variant="outline">Click here</Button>
      </div>
